cf16da392d6804fd8fc835c6f31b87af153502b2
lubukhu <150521080+lubukhu@users.noreply.github.com>
2025-08-02 01:30:26 +0700

    Chuyển scene

---
+++ Assets/Scripts/UI/Khoi/OutAnimationText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OutAnimationText : MonoBehaviour
+{
+    public Animator animator;
+    public void DisableAnimator()
+    {
+        if (animator != null)
+        {
+            animator.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Lỗi chưa gắng animator");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Khoi/Scene/LoadSceneOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public string sceneName;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
